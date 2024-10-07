eb8e7e96cfa1e6764dca26935f17571b8be3bd9a
alenashmelts <alenashmelts@yandex.ru>
2024-10-07 09:26:10 +0500

    add control panel

--- src/components/Set.jsx
+++ src/components/Set.jsx
@@ -3,14 +3,35 @@ import {Card} from "./Card";
 export function Set(){
     const cards = require('../data'); //подключить данные с data.json
     const[step, setStep] =useState(0);
+
+    const handleNext = () =>{
+        if(step < cards.length - 1){
+        setStep(step + 1)
+    }
+
+    }
+
+    const handlePrev = () =>{
+        if(step > 0){
+        setStep(step - 2)
+    }
+    }
+
+
+
     return(
 <div>
     <h2>Название набора</h2>
     <Card 
+    key = {cards[step].id}
     front={cards[step].front}
     back={cards[step].back}
     />
-    <div>элементы управлния</div>
+    <div className="control-panel">
+        <button disabled = {step == 0 ? true : false} onClick={handlePrev}> &lt; </button>
+        <p> {step + 1} / {cards.length} </p>
+        <button disabled = {step == cards.length - 1 ? true : false} onClick={handleNext}> &gt; </button>
+    </div>
 </div>
     );
 }
